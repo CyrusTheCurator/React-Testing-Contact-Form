0b17c5195f2c8a59f3aa45ddc889ea5254870965
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-03-19 22:07:26 -0700

    integrated basic initial testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,58 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("Checks that all input fields are present, properly labeled, and that no duplicates exist", () => {
+  const container = render(<App />);
+
+  container.getByTestId("firstName");
+  container.getByTestId("lastName");
+  container.getByTestId("email");
+  container.getByTestId("message");
+
+  container.getByText(/First Name/i);
+  container.getByText(/Last Name/i);
+  container.getByText(/Email/i);
+  container.getByText(/Message/i);
+  console.log("All input fields in place and appropriately labeled.");
+});
+
+// test("Button Behavior Test", () => {
+//   render(<App />);
+// });
+
+test("tests submit functionality", () => {
+  const container = render(<App />);
+
+  const firstNameField = container.getByTestId("firstName");
+  const lastNameField = container.getByTestId("lastName");
+  const emailField = container.getByTestId("email");
+  const messageField = container.getByTestId("message");
+
+  fireEvent.keyDown(firstNameField, { key: "A", code: 65, charCode: 65 });
+  fireEvent.keyDown(lastNameField, { key: "A", code: 65, charCode: 65 });
+  fireEvent.keyDown(emailField, { key: "A", code: 65, charCode: 65 });
+  fireEvent.keyDown(messageField, { key: "A", code: 65, charCode: 65 });
+
+  const submitButton = container.getByTestId("submit");
+
+  fireEvent.click(submitButton);
+
+  test("tests some THINGS", async () => {
+    const JSONdata = await wait(
+      () => container.getByTestId("JSON data element"),
+      { container }
+    );
+
+    expect(JSONdata.innerHTML).tobe({
+      firstName: "A",
+      lastName: "A",
+      email: "A",
+      message: "A"
+    });
+  });
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
+export let testData = {};
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit, reset } = useForm({
     mode: "onBlur"
   });
+
   const onSubmit = data => {
+    console.log(data);
     setData(data);
+    testData = data;
   };
 
   return (
@@ -19,6 +22,7 @@ const ContactForm = () => {
             name="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
+            data-testid="firstName"
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -31,6 +35,7 @@ const ContactForm = () => {
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
+            data-testid="lastName"
           />
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
@@ -41,21 +46,32 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input
+            name="email"
+            ref={register({ required: true })}
+            data-testid="email"
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            name="message"
+            ref={register({ required: false })}
+            data-testid="message"
+          />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre
+            data-testid="JSON data element"
+            style={{ textAlign: "left", color: "white" }}
+          >
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submit" type="submit" />
       </form>
     </div>
   );
